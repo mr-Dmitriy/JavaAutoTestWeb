0ec5120eb8ac21eb798a759af5e56430dfa75f02
Dmitriy_gb <d.o.prihodchenko@gmail.com>
2022-04-17 18:10:08 +0300

    Done HomeWork04_2

--- src/test/java/org/example/lesson04/TestForSquareOfTriangle.java
+++ src/test/java/org/example/lesson04/TestForSquareOfTriangle.java
@@ -1,8 +1,5 @@
 package org.example.lesson04;
 
-import org.example.lesson04.SquareOfTriangle;
-import org.example.lesson04.ExceptionForSquareOfTriangle;
-
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -36,27 +33,44 @@ public class TestForSquareOfTriangle {
      void testWithNegativeValues(int a, int b, int c) throws ExceptionForSquareOfTriangle {
         logger.info("a = " + a + "; b = " + b + "; с = " + c );
         SquareOfTriangle triangle = new SquareOfTriangle();
-        Assertions.assertThrows(ExceptionForSquareOfTriangle.class,()-> triangle.squareOfTriangle(a, b, c));
+        Assertions.assertThrows(ExceptionForSquareOfTriangle.class,()-> {triangle.squareOfTriangle(a, b, c);});
     }
 
+    @ParameterizedTest
+    @Tag("Negative")
+    @DisplayName("Test with null value ")
+    @CsvSource({"0,2,3",
+                "2,0,5",
+                "3,4,0"})
+    void testWithNullValues(int a, int b, int c) throws ExceptionForSquareOfTriangle {
+        logger.info("a = " + a + "; b = " + b + "; с = " + c );
+        SquareOfTriangle triangle = new SquareOfTriangle();
+        Assertions.assertThrows(ExceptionForSquareOfTriangle.class,()-> {triangle.squareOfTriangle(a, b, c);});
+    }
 
-
+    @ParameterizedTest
+    @Tag("Negative")
+    @DisplayName("Test with on of side that longer than sum of other side")
+    @CsvSource({
+                "2,3,6",
+                "7,2,1"})
+    void testOneSideLongerThanSumOfOther(int a, int b, int c)  {
+        logger.info("a = " + a + "; b = " + b + "; с = " + c );
+        SquareOfTriangle triangle = new SquareOfTriangle();
+        Assertions.assertThrows(ExceptionForSquareOfTriangle.class,()-> {triangle.squareOfTriangle(a, b, c);});
+    }
 
     @ParameterizedTest
     @Tag("Positive")
     @DisplayName("Comparing the square Calculation Result with the Expected")
-    @CsvSource({ "12,34,43,150.9252712437516",
+    @CsvSource({ "-12,34,43,150.9252712437516",
                  "43,23,34,388.84444190447164",
-                 "43,24,34,388.84444190447164"})
+                 "3,1,1,388.84444190447164"})
     void triangleDifferentSidesTest(int a, int b, int c, double s) throws ExceptionForSquareOfTriangle {
         logger.info("a = " + a + "; b = " + b + "; с = " + c + "; s = " + s );
         SquareOfTriangle triangle = new SquareOfTriangle();
         Assertions.assertEquals(s,triangle.squareOfTriangle(a, b, c));
     }
-
-
-
-
 
     @AfterAll
     static void AfterAll() {
